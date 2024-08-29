cf68e6c7190f1419ffc496c5fb789b17360c0991
smit <smitkanani45@gmail.com>
2024-08-29 19:00:10 +0530

    commit

---
+++ sentry.client.config.ts
@@ -0,0 +1,11 @@
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://96cb1ddce9983ceaf5ddfdc8c7c410e3@o4507854524252160.ingest.us.sentry.io/4507854768046080",
+  integrations: [
+    Sentry.feedbackIntegration({
+      // Additional SDK configuration goes in here, for example:
+      colorScheme: "system",
+    }),
+  ],
+});
