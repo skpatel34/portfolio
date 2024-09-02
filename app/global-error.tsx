004ed695c8228ae9894e0cade0ca2209c7c31bee
smit <smitkanani45@gmail.com>
2024-09-02 14:32:15 +0530

    f1

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import * as Sentry from "@sentry/nextjs";
+// import * as Sentry from "@sentry/nextjs";
 import NextError from "next/error";
 import { useEffect, } from "react";
 
 export default function GlobalError({ error }: { error: Error & { digest?: string } }) {
   useEffect(() => {
-    Sentry.captureException(error);
+    // Sentry.captureException(error);
   }, [error]);
 
   return (
